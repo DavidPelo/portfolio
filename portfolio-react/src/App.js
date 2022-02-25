541ce1514502ff9cb34cc7d5173161d500e95c61
davidwpelo <65370511+davidwpelo@users.noreply.github.com>
2022-02-25 09:15:48 -0600

    Update App.js

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,7 +1,5 @@
 import Container from './components/UI/Container'
 import NavBar from './components/UI/NavBar'
-import Labs from './components/UI/Labs'
-import Projects from './components/UI/Projects'
 import Welcome from './components/UI/Welcome'
 
 function App() {
