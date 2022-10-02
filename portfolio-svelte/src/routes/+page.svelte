1d992029c95bed0332775b4e971e745c4e52e4cd
davidpelo <65370511+DavidPelo@users.noreply.github.com>
2022-10-02 11:46:03 -0400

    Setup initial layout and styles

--- portfolio-svelte/src/routes/+page.svelte
+++ portfolio-svelte/src/routes/+page.svelte
@@ -1,5 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<main class="bg-slate-700 text-white">
+	<section class="mx-auto h-screen px-5">
+		<div class="flex h-1/3 items-end border-b border-yellow-300 pb-6">
+			<h1 class="text-3xl">David Pelo</h1>
+			<nav class="ml-auto flex flex-col">
+				<a class="pt-2 text-lg" id="projects" href="projects">projects</a>
+				<a class="pt-2 text-lg" id="#contact" href="contact">contact</a>
+				<a class="pt-2 text-lg" href="https://github.com/davidpelo" target="_blank" rel="noreferrer"
+					>github</a
+				>
+				<a
+					class="pt-2 text-lg"
+					href="https://www.linkedin.com/in/davidpelo"
+					target="_blank"
+					rel="noreferrer">linkedIn</a
+				>
+			</nav>
+		</div>
+		<div class="h-2/3 pt-6">
+			<h1 class="text-5xl">Welcome</h1>
+			<p class="pt-4 font-sans">
+				Ambitious and creative Web Developer with a passion for finding effective customer solutions
+				using JavaScript, HTML, CSS, and jQuery. Lifelong learner with a proven track record of
+				strong analytical, communication, and leadership skills that strives to grow professionally
+				through continuous study and participation within the industry.
+			</p>
+		</div>
+	</section>
+
+	<section class="mx-auto h-screen px-5">
+		<div class="flex h-20 items-center border-b border-yellow-300">
+			<h1 class="text-3xl">projects</h1>
+		</div>
+		<div class="my-5 flex h-5/6 items-center justify-center rounded-lg bg-slate-500">
+			project showcase
+		</div>
+	</section>
+
+	<section class="mx-auto h-screen px-5">
+		<div class="flex h-20 items-center border-b border-yellow-300">
+			<h1 class="text-3xl">contact</h1>
+		</div>
+		<div class="my-5 flex h-5/6 items-center justify-center rounded-lg bg-slate-500">
+			contact form
+		</div>
+	</section>
+</main>
