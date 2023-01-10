741ebbd016414841894994c2d5b4d087cca93afb
davidpelo <65370511+DavidPelo@users.noreply.github.com>
2023-01-10 13:55:40 -0500

    formatting

--- portfolio-react/src/components/UI/Welcome.js
+++ portfolio-react/src/components/UI/Welcome.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Container from "./Container";
+import React from 'react'
+import Container from './Container'
 
 function Welcome({ greeting, message, secondaryMessage }) {
   return (
-    <Container class="greetingContainer">
-      <h2 style={{"margin-bottom": "0.75rem"}}>{greeting}</h2>
-      <p style={{"margin-bottom": "0.75rem"}}>{message}</p>
+    <Container class='greetingContainer'>
+      <h2 style={{ 'margin-bottom': '0.75rem' }}>{greeting}</h2>
+      <p style={{ 'margin-bottom': '0.75rem' }}>{message}</p>
       {secondaryMessage && <p>{secondaryMessage}</p>}
     </Container>
-  );
+  )
 }
 
-export default Welcome;
+export default Welcome
