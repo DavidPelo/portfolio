31fea477a8d802af01c73b33f36020b86ac1a2b9
davidpelo <davidwpelo@gmail.com>
2023-01-13 15:26:00 -0500

    ran linter and formatter

--- portfolio-next/src/env/schema.mjs
+++ portfolio-next/src/env/schema.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { z } from "zod";
+import { z } from 'zod'
 
 /**
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  NODE_ENV: z.enum(["development", "test", "production"]),
-});
+  NODE_ENV: z.enum(['development', 'test', 'production'])
+})
 
 /**
  * Specify your client-side environment variables schema here.
@@ -16,4 +16,4 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
-});
+})
